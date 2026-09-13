da6eaac3fe92d2aa7bf631210a7bc6c17cb2ee68
Adithya K S <adxthyaks662@gmail.com>
2026-09-13 13:20:41 +0530

    Add Man class with name and age attributes

---
+++ Man.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class Man
+{
+    String name;
+    int age;
+
+    public Man(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayMe()
+    {
+        System.out.println("Man's Name: " + name
+                         + ", Age: " + age);
+    }
+}
